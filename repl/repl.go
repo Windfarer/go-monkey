ee6f8b7bf49b1a7040331229b59937417ee2bdd8
Windfarer <windfarer@gmail.com>
2022-08-13 17:31:47 +0800

    ch2

---
+++ repl/repl.go
@@ -0,0 +1,59 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"monkey/compiler"
+	"monkey/lexer"
+	"monkey/parser"
+	"monkey/vm"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+
+		comp := compiler.New()
+		if err := comp.Compile(program); err != nil {
+			fmt.Fprintf(out, "Compilation failed:\n %s\n", err)
+			continue
+		}
+
+		machine := vm.New(comp.ByteCode())
+		if err := machine.Run(); err != nil {
+			fmt.Fprintf(out, "Executing byecode failed:\n %s\n", err)
+			continue
+		}
+
+		stackTop := machine.StackTop()
+		io.WriteString(out, stackTop.Inspect())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
