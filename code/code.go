2f1c259eab61d195c107e4256d0a521715df504a
Windfarer <windfarer@gmail.com>
2022-08-27 14:52:00 +0800

    temp

--- code/code.go
+++ code/code.go
@@ -29,6 +29,9 @@ const (
 
 	OpMinus
 	OpBang
+
+	OpJumpNotTruthy
+	OpJump
 )
 
 type Definition struct {
@@ -55,6 +58,9 @@ var definitions = map[Opcode]*Definition{
 
 	OpMinus: {"OpMinus", []int{}},
 	OpBang:  {"OpBang", []int{}},
+
+	OpJumpNotTruthy: {"OpJumpNotTruthy", []int{2}},
+	OpJump: {"OpJump", []int{2}},
 }
 
 func Lookup(op byte) (*Definition, error) {
